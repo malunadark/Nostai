dc265b891af3e0b327afb95ba511bd0d9285920c
Татьяна <93919072+malunadark@users.noreply.github.com>
2025-08-11 23:16:29 +0300

    Update chronicles.js

--- chronicles.js
+++ chronicles.js
@@ -1,6 +1,22 @@
-$(function() {
-  var pageTurnSound = document.getElementById('page-turn-sound');
+// Функция для динамической загрузки скрипта с колбэками
+function loadScript(src) {
+  return new Promise(function(resolve, reject) {
+    if (document.querySelector(`script[src="${src}"]`)) {
+      // Скрипт уже загружен
+      resolve();
+      return;
+    }
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    script.onload = () => resolve();
+    script.onerror = () => reject(new Error(`Не удалось загрузить скрипт: ${src}`));
+    document.head.appendChild(script);
+  });
+}
 
+function initBook() {
+  var pageTurnSound = document.getElementById('page-turn-sound');
   $('#book').turn({
     width: 900,
     height: 600,
@@ -13,4 +29,21 @@ $(function() {
       pageTurnSound.play();
     }
   });
-});
+}
+
+// Последовательная загрузка скриптов
+loadScript('https://code.jquery.com/jquery-3.6.0.min.js')
+  .then(() => loadScript('js/turn.min.js'))
+  .then(() => {
+    // Ждём пока DOM будет готов и jQuery будет доступен
+    if (window.jQuery) {
+      $(function() {
+        initBook();
+      });
+    } else {
+      console.error('jQuery не найден после загрузки');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
