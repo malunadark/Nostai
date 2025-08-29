1f6e1981ddcb285f26c7924e6b3c3aae6225e5eb
Татьяна <93919072+malunadark@users.noreply.github.com>
2025-08-29 16:42:48 +0300

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,10 @@
 export function setupParallax(container) {
-  container.style.background = "url('assets/images/Nostai.png') no-repeat center center fixed";
+  container.style.background = "url('assets/images/Nostai.png') no-repeat center center";
   container.style.backgroundSize = "cover";
-  
-  // Простейший JS параллакс по движению мыши
-  container.addEventListener("mousemove", (e) => {
-    const moveX = (e.clientX / window.innerWidth - 0.5) * 10;
-    const moveY = (e.clientY / window.innerHeight - 0.5) * 10;
-    container.style.backgroundPosition = `${50 - moveX}% ${50 - moveY}%`;
+
+  window.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 20;
+    const y = (e.clientY / window.innerHeight - 0.5) * 20;
+    container.style.backgroundPosition = `${50 + x}% ${50 + y}%`;
   });
 }
