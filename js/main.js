25b9e1aa7d00837a7a4f969ac8ef1bdd78012b5d
Татьяна <93919072+malunadark@users.noreply.github.com>
2025-08-29 15:26:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-import './door-scene.js';
-import './parallax.js';
-import './runes.js';
-import './smoke.js';
-import './music.js';
+import "./parallax.js";
+import "./runes.js";
+import "./door-scene.js";
+import "./doors.js";
+import "./script.js";
+import "./smoke.js";
+import "./music.js";
+// turn.min.js можно подключить напрямую в HTML или тут, если хочешь.
