1b1a32c0ab6b5d36a9576b39488c4814e9add8a7
Татьяна <93919072+malunadark@users.noreply.github.com>
2025-08-29 15:36:52 +0300

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,11 +1,17 @@
 const music = document.getElementById('bg-music');
-let isPlaying = true;
+let audio;
 
-document.body.addEventListener('click', () => {
-  if (isPlaying) {
-    music.pause();
-  } else {
-    music.play();
-  }
-  isPlaying = !isPlaying;
-});
+export function setupMusic() {
+  audio = new Audio("assets/audio/Уходящая Судьба.mp3");
+  audio.loop = true;
+  audio.volume = 0.5;
+  audio.play().catch(() => {
+    console.log("Авто-воспроизведение заблокировано браузером");
+  });
+}
+
+export function toggleMusic() {
+  if (!audio) return;
+  if (audio.paused) audio.play();
+  else audio.pause();
+}
